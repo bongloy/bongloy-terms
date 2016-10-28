3e1f780fbad10f8d32ea3e6d1d7b93a05ba02104
David Wilkie <dwilkie@gmail.com>
2016-10-28 15:33:55 +0700

    Add switches for rendering terms, privacy and prohibited businesses according to configuration

--- lib/bongloy/terms/renderer.rb
+++ lib/bongloy/terms/renderer.rb
@@ -1,6 +1,18 @@
 require "redcarpet"
 
 class Bongloy::Terms::Renderer
+  def self.render_terms?
+    do_render?(:terms)
+  end
+
+  def self.render_privacy?
+    do_render?(:privacy)
+  end
+
+  def self.render_prohibited_businesses?
+    do_render?(:prohibited_businesses)
+  end
+
   def terms(country = :kh, locale = :en)
     render_markdown("#{country}_terms", locale)
   end
@@ -11,6 +23,18 @@ class Bongloy::Terms::Renderer
 
   def prohibited_businesses(country = :kh, locale = :en)
     render_markdown("#{country}_prohibited_businesses", locale)
+  end
+
+  def render_terms?
+    self.class.render_terms?
+  end
+
+  def render_privacy?
+    self.class.render_privacy?
+  end
+
+  def render_prohibited_businesses?
+    self.class.render_prohibited_businesses?
   end
 
   private
@@ -29,5 +53,10 @@ class Bongloy::Terms::Renderer
 
   def path_to_root(filename)
     File.join(File.expand_path('../..', File.dirname(__dir__)), filename)
+  end
+
+  def self.do_render?(key)
+    configuration = ENV["BONGLOY_TERMS_RENDER_#{key.to_s.upcase}"]
+    configuration.nil? || configuration.to_i != 0
   end
 end
--- spec/bongloy/terms/renderer_spec.rb
+++ spec/bongloy/terms/renderer_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Bongloy::Terms::Renderer do
+  include EnvHelpers
+
   describe "#terms(country, locale)" do
     it { expect(subject.terms).to be_a(String) }
   end
@@ -11,5 +13,32 @@ describe Bongloy::Terms::Renderer do
 
   describe "#prohibited_businesses(country, locale)" do
     it { expect(subject.prohibited_businesses).to be_a(String) }
+  end
+
+  [:terms, :privacy, :prohibited_businesses].each do |page|
+    it { is_expected.to respond_to(:"render_#{page}?") }
+
+    describe ".render_#{page}?" do
+      let(:result) { described_class.public_send("render_#{page}?") }
+
+      before do
+        stub_env("BONGLOY_TERMS_RENDER_#{page.to_s.upcase}" => configuration)
+      end
+
+      context "not specified" do
+        let(:configuration) { nil }
+        it { expect(result).to eq(true) }
+      end
+
+      context "turned on" do
+        let(:configuration) { "1" }
+        it { expect(result).to eq(true) }
+      end
+
+      context "turned off" do
+        let(:configuration) { "0" }
+        it { expect(result).to eq(false) }
+      end
+    end
   end
 end
